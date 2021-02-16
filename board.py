e1e86bd704c009f7db8fe3a37e2926e9d4e32720
b30wulffz <shlokpandey123@gmail.com>
2021-02-16 08:43:25 +0530

    Add wall

--- board.py
+++ board.py
@@ -6,7 +6,7 @@ class Board():
 
     def __init__(self, bricks=[], balls=[]):
         self.cols = 80
-        self.rows = 35
+        self.rows = 32
         self.paddle = Paddle(self.cols//2 -6 , self.rows-1) # moving paddle to center
         self.ball = Ball(self.cols//2 - 2, self.rows-2) # moving ball on top of paddle
         self.render()
@@ -15,6 +15,15 @@ class Board():
 
         system('clear')
         self.board = [['.' for i in range(self.cols)] for j in range(self.rows)]
+        # self.board = []
+        # for j in range(self.rows+2):
+        #     row = []
+        #     for i in range(self.cols+2):
+        #         if(i==0 or i==self.cols+1):
+        #             row.append('|')
+        #         else:
+        #             row.append('.')
+        #     self.board.append(row)
 
         # render paddle
         for row in range(self.paddle.y, self.paddle.y+self.paddle.height):
@@ -30,8 +39,30 @@ class Board():
         
         # left wall collision
 
+        # right wall collision
+
+        # top wall collision
+
+        # bottom wall collision
+
+        # paddle collision
+
+        # brick collision
+
+        # if collision update its position to collision location and update velocity
+
         for row in range(self.ball.y, self.ball.y+self.ball.height):
             for col in range(self.ball.x, self.ball.x+self.ball.width):
                 self.board[row][col] = self.ball.pixel
 
-        print("\n".join(["".join(row) for row in self.board]))
+        # adding borders to board
+        height = 4
+        width= self.cols
+        wall = 1
+        self.output = [['#' for i in range(width+2*wall)] for j in range(height+2*wall)]
+        self.output.extend([['#' for i in range(self.cols+2*wall)] for j in range(self.rows+2*wall)])
+        for j in range(wall, self.rows+wall):
+            for i in range(wall, self.cols+wall):
+                self.output[j+height+wall+1][i] = self.board[j-wall][i-wall]
+
+        print("\n".join(["".join(row) for row in self.output]))
